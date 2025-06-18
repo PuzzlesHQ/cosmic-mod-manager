94df70bd23e6eaa822e1bbafe7ac63c0516ed376
Abhinav5383 <itsabhinav5383@gmail.com>
2025-06-18 22:27:47 +0530

    Fix frontend crashing due to icon import

--- packages/components/src/icons/tag-icons.tsx
+++ packages/components/src/icons/tag-icons.tsx
@@ -17,7 +17,6 @@ import {
     CompassIcon,
     CpuIcon,
     DollarSignIcon,
-    DoorClosedLockedIcon,
     EarthIcon,
     FeatherIcon,
     FilmIcon,
@@ -60,9 +59,8 @@ import {
     ZapIcon,
 } from "lucide-react";
 import type { JSX } from "react";
-import type { IconSvgProps } from "../types";
+import type { IconSvgProps } from "~/types";
 import { CubeIcon, DefaultSvgSize } from "./index";
-
 export const PuzzleIcon: React.FC<IconSvgProps> = ({ size, width, height, ...props }) => {
     return (
         <svg
@@ -408,6 +406,30 @@ export const IslandIcon: React.FC<IconSvgProps> = ({ size, width, height, ...pro
                     </g>
                 </g>
             </g>
+        </svg>
+    );
+};
+
+export const DoorClosedLockedIcon: React.FC<IconSvgProps> = ({ size, width, height, ...props }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            aria-hidden="true"
+            width={size || width || DefaultSvgSize}
+            height={size || height || DefaultSvgSize}
+            {...props}
+        >
+            <path d="M10 12h.01" />
+            <path d="M18 9V6a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v14" />
+            <path d="M2 20h8" />
+            <path d="M20 17v-2a2 2 0 1 0-4 0v2" />
+            <rect x="14" y="17" width="8" height="5" rx="1" />
         </svg>
     );
 };
