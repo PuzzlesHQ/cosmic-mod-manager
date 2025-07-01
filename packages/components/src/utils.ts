bad07a212ef8bb3ee38ad4d8fcf3f4e3463b7aff
Abhinav5383 <itsabhinav5383@gmail.com>
2025-07-01 14:10:32 +0530

    Fix custom twMerge not working correctly with custom property values

--- packages/components/src/utils.ts
+++ packages/components/src/utils.ts
@@ -1,6 +1,24 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+const properties = ["p", "px", "py", "pt", "pb", "ps", "pe"];
+
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
+    const reversedInputs = inputs.filter((i) => typeof i === "string").reverse();
+    const modifiedInputs: ClassValue[] = [];
+    const seenProperties: string[] = [];
+
+    outer: for (const classVal of reversedInputs) {
+        for (const prop of properties) {
+            if (classVal.trim().startsWith(`${prop}-`) || classVal.trim().startsWith(`!${prop}-`)) {
+                if (seenProperties.includes(prop)) continue outer;
+                else seenProperties.push(prop);
+            }
+        }
+
+        modifiedInputs.push(classVal);
+    }
+    modifiedInputs.reverse();
+
+    return twMerge(clsx(modifiedInputs));
 }
