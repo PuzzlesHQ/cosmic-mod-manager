11a796070047179e88de87c79c665369f6eaeaa5
Abhinav5383 <itsabhinav5383@gmail.com>
2025-07-07 17:19:07 +0530

    increate about page card padding

--- apps/frontend/app/routes/about.tsx
+++ apps/frontend/app/routes/about.tsx
@@ -9,7 +9,7 @@ export default function () {
     return (
         <main className="grid w-full grid-cols-1">
             <MarkdownRenderBox
-                className="bright-heading mx-auto max-w-[80ch] rounded-lg bg-card-background p-card-surround"
+                className="bright-heading mx-auto max-w-[80ch] rounded-lg bg-card-background p-10 pt-4"
                 text={t.legal.aboutUs({
                     discordInvite: Config.DISCORD_INVITE,
                     repoLink: Config.REPO_LINK,
