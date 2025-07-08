11af6a15651231a63c371252cd205d1fc9e4aef5
Abhinav5383 <itsabhinav5383@gmail.com>
2025-07-08 19:07:58 +0530

    bundle more chunks together

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,11 +5,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { ASSETS_URL } from "./app/utils/server-config";
 
-const CustomSrcChunks = [
-    ["utils-extras", ["packages/utils/src"]],
-    ["app-layout-components", ["apps/frontend/app/components/layout"]],
-] as const;
-
 export default defineConfig({
     server: {
         port: 3000,
@@ -19,39 +14,25 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks(id) {
-                    // Vendor
                     if (id.includes("node_modules")) {
-                        // Special cases
-                        if (id.includes("radix-ui")) return "radix-ui";
-                        if (id.includes("highlight.js") || id.includes("xss")) return "md-renderer";
-                        if (id.includes("markdown-it")) return "markdown-it";
-
-                        const parts = id.split("/node_modules/")[1].split("/");
-                        if (id.includes("/react-router/")) return `vendor/react-router-${parts[1]}`;
-
-                        return `vendor/${parts[0]}`;
-                    }
-
-                    // Utils package
-                    if (id.includes("packages/utils/src")) {
-                        const parts = id.split("/packages/utils/src/")[1].split("/");
-                        if (!parts[1]) return "utils/core";
-                        return `utils/${parts[0]}`;
-                    }
-
-                    // Styles
-                    if (id.endsWith(".css")) return "styles";
-
-                    // Icons
-                    if (id.includes("components/icons/tag-icons")) return "tag-icons";
-                    if (id.includes("components/icons")) return "icons";
-
-                    for (let i = 0; i < CustomSrcChunks.length; i++) {
-                        const [chunkName, paths] = CustomSrcChunks[i];
-
-                        for (const path of paths) {
-                            if (id.includes(path)) return chunkName;
+                        if (id.includes("d3-")) return "vendor/d3";
+                        if (id.includes("/react-router/")) return "vendor/react-router";
+                        if (id.includes("highlight") || id.includes("xss") || id.includes("markdown-it")) {
+                            return "vendor/md-renderer";
                         }
+                        return "vendor";
+                    }
+                    // Styles
+                    else if (id.endsWith(".css")) return "styles";
+                    // Utils package
+                    else if (id.includes("packages/utils")) return "pkg-utils";
+                    // Components
+                    else if (id.includes("app/components")) {
+                        if (id.includes("icons")) return "icons";
+                        // if (id.includes("app/components/ui")) return "components-ui";
+                        // if (id.includes("app/components/misc")) return "components-misc";
+                        // if (id.includes("app/components/md-editor")) return "components-md-editor";
+                        if (id.includes("app/components/")) return "components";
                     }
                 },
             },
