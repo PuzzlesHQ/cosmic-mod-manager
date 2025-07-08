db7e20068fa0a3a7245d282695f6b46167349c7f
Abhinav5383 <itsabhinav5383@gmail.com>
2025-07-08 23:15:13 +0530

    refactor: improve manual chunking logic in vite config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -13,27 +13,84 @@ export default defineConfig({
     build: {
         rollupOptions: {
             output: {
+                experimentalMinChunkSize: 307200, // 300 KiB
                 manualChunks(id) {
+                    // Vendor
                     if (id.includes("node_modules")) {
+                        // Special cases
                         if (id.includes("d3-")) return "vendor/d3";
-                        if (id.includes("/react-router/")) return "vendor/react-router";
-                        if (id.includes("highlight") || id.includes("xss") || id.includes("markdown-it")) {
+                        if (id.includes("radix-ui")) return "vendor/radix-ui";
+
+                        if (
+                            [
+                                "punycode",
+                                "uc.micro",
+                                "eventemitter",
+                                "internmap",
+                                "tiny-invariant",
+                                "decimal.js",
+                                "lodash",
+                                "get-nonce",
+                                "tslib",
+                                "ua-parser-js",
+                                "turbo-stream",
+                                "scheduler",
+                            ].some((pkg) => id.includes(pkg))
+                        ) {
+                            return "vendor/misc";
+                        }
+
+                        if (["tailwind-merge", "clsx", "class-variance-authority"].some((pkg) => id.includes(pkg))) {
+                            return "vendor/tailwind";
+                        }
+
+                        if (
+                            [
+                                "react-remove",
+                                "react-is",
+                                "aria-hidden",
+                                "@hookform",
+                                "react-hook-form",
+                                "react-style",
+                                "@floating-ui",
+                                "cmdk",
+                                "fast-equals",
+                                "prop-types",
+                                "react-smooth",
+                                "sonner",
+                            ].some((pkg) => id.includes(pkg))
+                        ) {
+                            return "vendor/react-libs";
+                        }
+
+                        if (id.includes("recharts")) return "vendor/recharts";
+
+                        if (
+                            ["highlight.js", "xss", "markdown-it", "cssfilter", "mdurl", "linkify-it"].some((pkg) =>
+                                id.includes(pkg),
+                            )
+                        ) {
                             return "vendor/md-renderer";
                         }
-                        return "vendor";
+
+                        if (id.includes("/react-router/")) return "vendor/react-router";
+
+                        const parts = id.split("/node_modules/")[1].split("/");
+                        return `vendor/${parts[0]}`;
                     }
-                    // Styles
-                    else if (id.endsWith(".css")) return "styles";
+
                     // Utils package
-                    else if (id.includes("packages/utils")) return "pkg-utils";
-                    // Components
-                    else if (id.includes("app/components")) {
-                        if (id.includes("icons")) return "icons";
-                        // if (id.includes("app/components/ui")) return "components-ui";
-                        // if (id.includes("app/components/misc")) return "components-misc";
-                        // if (id.includes("app/components/md-editor")) return "components-md-editor";
-                        if (id.includes("app/components/")) return "components";
-                    }
+                    if (id.includes("packages/utils/src")) return "pkg-utils";
+
+                    // Styles
+                    if (id.endsWith(".css")) return "styles";
+
+                    // Icons
+                    if (id.includes("app/components/icons/tag-icons")) return "tag-icons";
+                    if (id.includes("app/components/icons")) return "icons";
+                    if (id.includes("app/components/ui")) return "ui-components";
+                    if (id.includes("app/components/misc")) return "misc-components";
+                    if (id.includes("app/components")) return "components";
                 },
             },
         },
