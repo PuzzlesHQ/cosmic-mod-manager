ac025a0a6fde5b8514ca51b4511228529f42afcc
Abhinav5383 <itsabhinav5383@gmail.com>
2025-11-23 09:55:59 +0530

    update the prisma client version

---
+++ apps/backend/prisma.config.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+    datasource: {
+        url: env("PG_DATABASE_URL"),
+    },
+    experimental: {
+        extensions: true,
+    },
+});
