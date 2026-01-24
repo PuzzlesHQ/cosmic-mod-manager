cd0f29bc1bfab4c24d3d51ce08c06ff89d6c7ff7
Abhinav5383 <itsabhinav5383@gmail.com>
2026-01-24 16:28:57 +0530

    biome format

--- apps/frontend/app/pages/dashboard/organization/page.tsx
+++ apps/frontend/app/pages/dashboard/organization/page.tsx
@@ -37,7 +37,8 @@ export default function OrganisationDashboardPage({ organisations }: Props) {
                 ) : (
                     <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
                         {organisations?.map((org) => {
-                            if (org.members.find((member) => member.userId === session?.id)?.accepted === false) return null;
+                            if (org.members.find((member) => member.userId === session?.id)?.accepted === false)
+                                return null;
 
                             return (
                                 <OrgListItemCard
