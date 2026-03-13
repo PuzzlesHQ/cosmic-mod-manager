26f3a07f9c6b414f0eae0e0c67b9fcf937aa4ce8
Abhinav5383 <itsabhinav5383@gmail.com>
2026-03-13 10:01:01 +0530

    refactor email service to use environment variables for SMTP configuration

--- apps/backend/src/services/email.ts
+++ apps/backend/src/services/email.ts
@@ -2,12 +2,12 @@ import * as nodemailer from "nodemailer";
 import env from "~/utils/env";
 
 const noReplyEmailTransporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
+    host: env.NOREPLY_SMTP_RELAY,
+    port: Number.parseInt(env.NOREPLY_SMTP_PORT, 10) || 587,
     secure: false,
     auth: {
-        user: env.BREVO_USER,
-        pass: env.NOREPLY_EMAIL_PASSWORD,
+        user: env.NOREPLY_SMTP_USER,
+        pass: env.NOREPLY_SMTP_KEY,
     },
 });
 
@@ -20,7 +20,7 @@ type sendEmailOptions = {
 
 export async function sendEmail({ receiver, subject, text, template }: sendEmailOptions) {
     return await noReplyEmailTransporter.sendMail({
-        from: env.NOREPLY_EMAIL,
+        from: env.SUPPORT_NOREPLY_EMAIL,
         to: receiver,
         subject: subject,
         text: text || "",
