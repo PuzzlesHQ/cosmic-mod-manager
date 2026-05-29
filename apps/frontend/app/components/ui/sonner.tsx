9187cb3e8adf5fae7b5feb8a0aa3e665febf5ca1
Abhinav5383 <itsabhinav5383@gmail.com>
2026-05-29 23:29:58 +0530

    fix toast theme

--- apps/frontend/app/components/ui/sonner.tsx
+++ apps/frontend/app/components/ui/sonner.tsx
@@ -6,16 +6,13 @@ export { toast } from "sonner";
 export function Toaster(props: React.ComponentProps<typeof Sonner>) {
 	return (
 		<Sonner
-			theme={undefined}
-			className="toaster group"
 			toastOptions={{
 				classNames: {
-					toast:
-						"text-base group toast group-[.toaster]:bg-card-background group-[.toaster]:text-foreground-bright group-[.toaster]:border group-[.toaster]:border-border",
-					title: "leading-tight text-sm",
-					description: "text-sm !text-foreground-muted",
-					actionButton: "!bg-accent-bg !text-accent-bg-foreground",
-					cancelButton: "!bg-raised-background !text-foreground-muted",
+					toast: "bg-card-background! text-base! text-foreground-bright! border-border!",
+					title: "leading-tight! text-sm!",
+					description: "text-sm! text-foreground-muted!",
+					actionButton: "bg-accent-bg! text-accent-bg-foreground!",
+					cancelButton: "bg-raised-background! text-foreground-muted!",
 				},
 				duration: 10000,
 			}}
