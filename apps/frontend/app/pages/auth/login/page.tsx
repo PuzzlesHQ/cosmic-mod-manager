510bebcc1a43ba78f888d839d40316cf20401cb1
Abhinav5383 <itsabhinav5383@gmail.com>
2026-06-07 12:12:48 +0530

    fix missing param for LoginCard

--- apps/frontend/app/pages/auth/login/page.tsx
+++ apps/frontend/app/pages/auth/login/page.tsx
@@ -1,9 +1,12 @@
+import { useId } from "react";
 import { LoginPageCard } from "./login-card";
 
 export default function LoginPage() {
+    const id = useId();
+
     return (
         <main className="full_page flex w-full items-center justify-center py-12">
-            <LoginPageCard id="" />
+            <LoginPageCard id={id} />
         </main>
     );
 }
