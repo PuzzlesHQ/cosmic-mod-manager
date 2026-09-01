2d539e146c61ca1bfdf89c1da6193d4ad0dccdb1
Abhinav5383 <itsabhinav5383@gmail.com>
2026-09-01 13:04:52 +0530

    Improve query

--- apps/backend/prisma/sql/getMostStorageUsedProjects.sql
+++ apps/backend/prisma/sql/getMostStorageUsedProjects.sql
@@ -1,38 +1,37 @@
-WITH ProjectFiles AS (
+WITH ProjectFileSizes AS (
     SELECT
         v."projectId",
-        f.size
+        SUM(f.size) AS size
     FROM "VersionFile" vf
     JOIN "Version" v ON v.id = vf."versionId"
     JOIN "File" f ON f.id = vf."fileId"
+    GROUP BY v."projectId"
 
     UNION ALL
 
     SELECT
         g."projectId",
-        f.size
+        SUM(f.size) AS size
     FROM "GalleryItem" g
     JOIN "File" f ON f.id = g."imageFileId"
+    GROUP BY g."projectId"
 
     UNION ALL
 
     SELECT
         g."projectId",
-        f.size
+        SUM(f.size) AS size
     FROM "GalleryItem" g
     JOIN "File" f ON f.id = g."thumbnailFileId"
+    GROUP BY g."projectId"
 )
 SELECT
     p.id,
     p.name,
     p.slug,
-    SUM(pf.size) AS total_size
-FROM ProjectFiles pf
-JOIN "Project" p ON p.id = pf."projectId"
-GROUP BY
-    p.id,
-    p.name,
-    p.slug
-ORDER BY
-    total_size DESC
+    SUM(pfs.size) AS total_size
+FROM ProjectFileSizes pfs
+JOIN "Project" p ON p.id = pfs."projectId"
+GROUP BY p.id, p.name, p.slug
+ORDER BY total_size DESC
 LIMIT 5;
